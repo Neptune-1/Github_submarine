8d60e5ed6631d6a52ad1b3b748703c66bb909539
Alexandr <37707797+Neptune-1@users.noreply.github.com>
2019-08-04 16:22:20 +0300

    Update client_comp-v9.py

--- local/client_comp-v9.py
+++ local/client_comp-v9.py
@@ -15,7 +15,7 @@ sock = socket.socket()
 im=cv2.imread('1.jpg')
 while err == 0:
     try:
-        sock.connect(('192.168.43.237', 9001))
+        sock.connect(('192.168.0.37', 9001))
         err = 1
         print('connect')
 
